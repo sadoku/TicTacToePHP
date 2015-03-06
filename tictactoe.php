5fb427cc44846bbe828b2700b54794f249c6c106
Sadoku <derek.v.richard@gmail.com>
2015-03-05 19:03:40 -0500

    Updated Efficiency
    Modified table to be point based instead of coordinate based.

--- tictactoe.php
+++ tictactoe.php
@@ -3,12 +3,12 @@
  * TicTacToe Object
  *
  * LICENSE:
- *   TicTacToePHP Program is free software: you can redistribute it and/or modify
+ *   TicTacToe Program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
  *   the Free Software Foundation, either version 3 of the License, or
  *   (at your option) any later version.
  *
- *   TicTacToePHP Program is distributed in the hope that it will be useful,
+ *   TicTacToe Program is distributed in the hope that it will be useful,
  *   but WITHOUT ANY WARRANTY; without even the implied warranty of
  *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  *   GNU General Public License for more details.
@@ -31,88 +31,56 @@ class tictactoe{
 	function __construct($player){
 		$this->player = $player;
 		$this->totalMoves = 0;
-		$this->board = array(array(1,2,3),array(4,5,6),array(7,8,9));
+		$this->board = array(0,1,2,3,4,5,6,7,8);
 		$this->isDone = false;
 	}
 	
-	public function move($cords){
-		$cord = explode("_", $cords);
-		$this->board[$cord[0]][$cord[1]] = $this->player;
+	public function move($cord){
+		$this->board[$cord] = $this->player;
 		$this->player = ($this->player == "X" ? "O" : "X");
 		$this->totalMoves++;
-		if ($this->checkGame() !== null){
+		if ($this->checkGame() != null){
 			$this->isDone = true;
 		}
 	}
 	private function checkGame(){
-		
-		//Across the top X & X ==  X & X 
-		if ($this->board[0][0] == $this->board[0][1] && $this->board[0][1] == $this->board[0][2]){
-			return $this->board[0][0];
-		}	
-		//Across the middle
-		if ($this->board[1][0] == $this->board[1][1] && $this->board[1][1] == $this->board[1][2]){
-			return $this->board[1][0];
-		}	
-		//Across the bottom
-		if ($this->board[2][0] == $this->board[2][1] && $this->board[2][1] == $this->board[2][2]){
-			return $this->board[2][0];
-		}	
-		//Straight Left
-		if ($this->board[0][0] == $this->board[1][0] && $this->board[1][0] == $this->board[2][0]){
-			return $this->board[0][0];
-		}	
-		//Straight middle
-		if ($this->board[0][1] == $this->board[1][1] && $this->board[1][1] == $this->board[2][1]){
-			return $this->board[0][1];
-		}	
-		//Straight Right
-		if ($this->board[0][2] == $this->board[1][2] && $this->board[1][2] == $this->board[2][2]){
-			return $this->board[0][2];
-		}	
-		//Diagonal \
-		if ($this->board[0][0] == $this->board[1][1] && $this->board[1][1] == $this->board[2][2]){
-			return $this->board[0][0];
-		}	
-		//Diagonal /
-		if ($this->board[0][2] == $this->board[1][1] && $this->board[1][1] == $this->board[2][0]){
-			return $this->board[0][2];
+		$possSolutions = array(
+			array(0,1,2),array(3,4,5),array(6,7,8),
+			array(0,3,6),array(1,4,7),array(2,5,8),
+			array(0,4,8),array(2,4,6));
+		foreach($possSolutions as $point){
+			if ($this->board[$point[0]] == $this->board[$point[1]] && $this->board[$point[1]] == $this->board[$point[2]]){
+				return $this->board[$point[0]];
+			}
 		}
-		//Can only make 9 moves, otherwise its a tie.	
-		if ($this->totalMoves >= 8){
+		if ($this->totalMoves >= 9){
 			return "Tie";
-		}	
+		}
 	}
 	public function __toString(){
-		//Is the game over?
 		if (!$this->isDone){
 			echo "<table>";
-			
-			for ($x = 0; $x < 3; $x++){
-				echo "<tr>";
-				for ($y = 0; $y < 3; $y++){
-					echo "<td>";
-					//Is the position taken?
-					if(!is_int($this->board[$x][$y])){
-						$img = $this->board[$x][$y];
-						echo "<img src='img/$img.jpg' alt='$img' title='$img' />";
-					}
-					else{
-						//It's not? Let them choose this position
-						echo "<input type='radio' name='cords' value='{$x}_{$y}'></input>";
-					}
-					
-					echo "</td>";
+			for ($x = 0; $x < 9; $x++) {
+				if ($x == 3 || $x == 6) {
+					echo "</tr>";
 				}
-				
-				echo "</tr>";
+				if ($x == 0 || $x == 3) {
+					echo "<tr>";
+				}
+				if (!is_int($this->board[$x])) {
+					$img = $this->board[$x];
+					echo "<td><img src='img/$img.jpg' alt='$img' title='$img' /></td>";
+				} else {
+					//It's not? Let them choose this position
+					echo "<td><input type='radio' name='cords' value='{$x}'></input></td>";
+				}
 			}
 			return "</table><p><input type='submit' name='move' value='Submit' /><br/>It's player {$this->player}'s turn.</p>";
 		} else {
 			//Decide if there was a winner or not.
-			if($this->checkGame() != "Tie"){
+			if ($this->checkGame() != "Tie") {
 				echo $this->checkGame() . " has won the match!";
-			} elseif($this->checkGame() == "Tie"){
+			} elseif ($this->checkGame() == "Tie") {
 				echo "TIED MATCH";
 			} else {
 				echo "Something went wrong....";
