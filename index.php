146e1d6cada539e96124865622798db098008c12
sadoku <derek.v.richard@gmail.com>
2015-03-06 15:55:42 -0500

    Update index.php

--- index.php
+++ index.php
@@ -3,26 +3,32 @@ require_once('tictactoe.php');
 session_start();
 ?>
 <html>
-	<head>
-		<title>Tic-Tac-Toe</title>
-		<link rel="stylesheet" type="text/css" href="css/style.css" />
-	</head>
-	<body>
-		<div id="main">
-		<form action="game.php" method="POST">
-		<h2>Tic Tac Toe</h2>
-		<?php
-		if(isset($_SESSION['game'])){
-			echo unserialize($_SESSION['game']);
-		} else {
-		?>
-		 <select name="player">
-		  <option value="X">X</option>
-		  <option value="O">O</option>
-		</select>
-		<input type="submit" value="submit" name="submit"></input>
-		</form>
-		<?php } ?>
-		</div>
-	</body>
+<head>
+    <title>Tic-Tac-Toe</title>
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+</head>
+<body>
+<div id="main">
+    <form action="game.php" method="POST">
+        <h1>Tic Tac Toe</h1>
+        <?php
+        if(isset($_SESSION['game'])){
+            echo unserialize($_SESSION['game']);
+        } else {
+        ?>
+        <div class="table">
+        <h3>Choose which player goes first</h3>
+        <table>
+        <tr><td><input type="radio" name="player" value="X" id="x" />
+        <label for="x"><img src="img/X.jpg" /></label></td>
+        <td><input type="radio" name="player" value="O" id="o" />
+        <label for="o"><img src="img/O.jpg" /></label></td></tr>
+
+        <tr><td colspan="2"><input type="submit" value="submit" name="submit" /></td></tr>
+        </table>
+        </div>
+    </form>
+    <?php } ?>
+</div>
+</body>
 </html>
