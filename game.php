b0e48212cdf3af603cd66d2d8018ccb5feb7f644
sadoku <derek.v.richard@gmail.com>
2015-03-06 15:55:23 -0500

    Img Push

--- game.php
+++ game.php
@@ -2,28 +2,33 @@
 require_once('tictactoe.php');
 session_start();
 if(isset($_POST['newgame'])){
-	$_SESSION = array(); 
-	session_unset();
-	session_destroy();
-	header("Location: index.php"); 
-	exit();
+    $_SESSION = array();
+    session_unset();
+    session_destroy();
+    header("Location: index.php");
+    exit();
 }
 if(isset($_POST['submit'])){
-	$game = new tictactoe($_POST['player']);
-	$_SESSION['game'] = serialize($game);
-	header("Location: index.php"); /* Redirect browser */
-	exit();
-	//echo $game;
+	if(isset($_POST['player'])){
+		$game = new tictactoe($_POST['player']);
+		$_SESSION['game'] = serialize($game);
+		header("Location: index.php"); /* Redirect browser */
+		exit();
+	} else {
+		header("Location: index.php"); /* Redirect browser */
+		exit();
+	}
+    //echo $game;
 }
 if(isset($_SESSION['game'])){
-	$game = unserialize($_SESSION['game']);
-	if(isset($_POST['move']) && isset($_POST['cords'])){
-		$cords = $_POST['cords'];
-		$game->move($cords);
-	}
-	$_SESSION['game'] = serialize($game);
-	header("Location: index.php"); 
-	exit();
+    $game = unserialize($_SESSION['game']);
+    if(isset($_POST['cords'])){
+        $cords = $_POST['cords'];
+        $game->move($cords);
+    }
+    $_SESSION['game'] = serialize($game);
+    header("Location: index.php");
+    exit();
 }
 
 
